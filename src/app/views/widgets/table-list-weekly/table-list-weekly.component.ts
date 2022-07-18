0b691b1a4fc10d394c475fd2231078fed2196a13
dennisfilhan <filhandennis@gmail.com>
2022-07-18 16:07:39 +0700

    init weekly data

--- src/app/views/widgets/table-list-weekly/table-list-weekly.component.ts
+++ src/app/views/widgets/table-list-weekly/table-list-weekly.component.ts
@@ -2,7 +2,8 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ModalitySlot } from 'src/app/models/radiology/modality-slot';
 
 import * as moment from 'moment';
-import * as lodash from 'lodash'
+import { RadiologyService } from 'src/app/services/radiology/radiology.service';
+// import * as lodash from 'lodash'
 
 @Component({
   selector: 'app-table-list-weekly',
@@ -13,6 +14,7 @@ export class TableListWeeklyComponent implements OnInit {
 
   @Input() data: any[]
   @Input() dateSelected: Date
+  @Input() sectionSelected: any
   @Input() modalitySlots: ModalitySlot[];
 
   @Output() itemClick = new EventEmitter<any>()
@@ -27,22 +29,117 @@ export class TableListWeeklyComponent implements OnInit {
     {date: null, label: 'Minggu', value: '06'},
   ]
 
-  scheduleList: any[] = []
+  list: any[] = []
+  slotData: any[] = []
 
-  constructor() { }
+  mockData = [
+    [
+      {
+        fromTime: '00:00',
+        toTime: '01:30',
+        patientName: 'Patient 1',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+      {
+        fromTime: '05:00',
+        toTime: '07:30',
+        patientName: 'Patient 2',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+      {
+        fromTime: '10:00',
+        toTime: '10:30',
+        patientName: 'Patient 3',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+    ],
+    [
+      {
+        fromTime: '03:00',
+        toTime: '06:00',
+        patientName: 'Patient 1',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+      {
+        fromTime: '07:00',
+        toTime: '08:00',
+        patientName: 'Patient 2',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+    ],
+    [
+      {
+        fromTime: '05:00',
+        toTime: '06:00',
+        patientName: 'Patient 1',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+      {
+        fromTime: '10:00',
+        toTime: '12:00',
+        patientName: 'Patient 2',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+    ],
+    [
+      {
+        fromTime: '08:00',
+        toTime: '10:00',
+        patientName: 'Patient 1',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+      {
+        fromTime: '10:30',
+        toTime: '11:00',
+        patientName: 'Patient 2',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+    ],
+    [],
+    [
+      {
+        fromTime: '03:00',
+        toTime: '05:59',
+        patientName: 'Patient 1',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+      {
+        fromTime: '07:00',
+        toTime: '08:00',
+        patientName: 'Patient 2',
+        patientDob: '2002-01-01',
+        patientLocalMrNo: '0123456'
+      },
+    ],
+    []
+  ]
+
+  constructor(
+    private radiologyService : RadiologyService,
+  ) { }
 
   ngOnInit() {
     this.scheduleListGenerate()
   }
 
-  ngOnChanges(changes: any) {
+  ngOnChanges(_changes: any) {
     // console.log('weekly changes', changes)
     this.generateDayLabel()
   }
 
   scheduleListGenerate () {
     // this.scheduleList = this.data
-    this.getSchedules()
+    // this.getSchedules()
   }
 
   generateDayLabel () {
@@ -60,83 +157,97 @@ export class TableListWeeklyComponent implements OnInit {
     }
     weeks.shift()
     this.days = weeks
+    this.fetchSlotData()
     return this.days
   }
 
   async getSchedules() {
-    const slots = this.modalitySlots
-    console.log('slots', slots)
     const setToHour2Digit = (time : number) => ('0' + time).slice(-2);
-    let lastCaptureSlot : any = {};
-    let rowSpan = 0;
-    this.scheduleList = Array.from(Array(24).keys()).map(time => {
-      const hour2digit = setToHour2Digit(time)
-      const firstFromTime = hour2digit  + ':00'
-      const firstToTime = hour2digit + ':30'
-      const lastFromTime = hour2digit  + ':30'
-      const lastToTime = setToHour2Digit(time+1) + ':00'
-
-      const patientFirst : any = slots.find(s => 
-          moment(firstFromTime, 'hh:mm').isSameOrAfter(moment(s.from_time, 'hh:mm')) &&
-          moment(firstToTime, 'hh:mm').isSameOrBefore(moment(s.to_time, 'hh:mm'))
-        ) || {};
-      
-      if(patientFirst.patient_name && patientFirst.patient_name === lastCaptureSlot.patient){
-        lastCaptureSlot.rowSpan = Number(lastCaptureSlot.rowSpan) + 1;
-      }else{
-        rowSpan = 1
-      }
-           
-      const firstSlot = {
-          fromTime: firstFromTime,
-          toTime:  firstToTime,
-          patient: patientFirst.patient_name,
-          dob: patientFirst.patient_dob,
-          localMrNo: patientFirst.local_mr_no,
-          examination: patientFirst.modality_examination_name,
-          note: patientFirst.notes,
-          status: patientFirst.status,
-          rowSpan: lastCaptureSlot.patient && patientFirst.patient_name === lastCaptureSlot.patient ? null : rowSpan
-      }  
-      
-      lastCaptureSlot = firstSlot
-
-      const patientLast : any = slots.find(s => 
-          moment(lastFromTime, 'hh:mm').isSameOrAfter(moment(s.from_time, 'hh:mm')) &&
-          moment(lastToTime, 'hh:mm').isSameOrBefore(moment(s.to_time, 'hh:mm'))
-        ) || {};
-        
-      if(patientLast.patient_name && patientLast.patient_name === lastCaptureSlot.patient){
-        lastCaptureSlot.rowSpan = Number(lastCaptureSlot.rowSpan) + 1;       
-      } else {
-        rowSpan = 1;
-      }
-
-      
-      const lastSlot = {
-        fromTime: lastFromTime,
-        toTime: lastToTime,
-        patient: patientLast.patient_name,
-        dob: patientLast.patient_dob,
-        localMrNo: patientLast.local_mr_no,
-        examination: patientLast.modality_examination_name,
-        note: patientLast.notes,
-        status: patientLast.status,
-        rowSpan: lastCaptureSlot.patient && patientLast.patient_name === lastCaptureSlot.patient ? null : rowSpan
-     }
-
-     lastCaptureSlot = patientLast
-
-      return [ firstSlot, lastSlot ]
+    // const data = this.mockData
+    let data = this.slotData
+    data = data.map((x: any) => {
+      return x.map((y: any) => {
+        return {
+          fromTime: y.from_time,
+          toTime: y.to_time,
+          patientName: y.patient_name,
+          patientDob: y.patient_dob,
+          patientLocalMrNo: y.local_mr_no
+        }
+      })
     })
 
-    console.log('this.scheduleList', this.scheduleList)
-
-    this.scheduleList = lodash.flatten(this.scheduleList)
+    const seeks: any = {}
+    const list = Array(24).fill({}).map((_m: any, i: number) => {
+      const model = new SlotWeeklyRow()
+      model.viewIndex = i
+      model.fromTime = setToHour2Digit(i) + ':00'
+      model.toTime = setToHour2Digit(i) + ':29'
+      model.days = this.days.map((_n: any, j: number) => {
+        let day = new SlotWeeklyItem
+        day.fromTime = model.fromTime
+        day.toTime = model.toTime
+        day.date = _n.date
+        day.rowSpan = 1
+        const seekForItem = data[j].find((_k: any) => {
+          return (
+            moment(day.fromTime, 'hh:mm').isSameOrAfter(moment(_k.fromTime, 'hh:mm')) &&
+            moment(day.toTime, 'hh:mm').isSameOrBefore(moment(_k.toTime, 'hh:mm'))
+          )
+        })
+        if (seekForItem) {
+          day = {...day, ...seekForItem}
+          const key = `${j}_${seekForItem.patientName}`
+          if (!seeks[key]) { seeks[key] = 0 }
+          seeks[key] += 1
+        }
+        return day
+      })
+      // console.log('rowSnap', {seeks})
+      return model
+    })
+    // console.log('list', list)
+    this.list = list
   }
 
   toDaily (val: any) {
     this.itemClick.emit(val)
   }
 
+  async getModalitySlots(reserveDate: any, sectionId: any) {
+    if (reserveDate && sectionId) {
+      reserveDate = moment(reserveDate).format('YYYY-MM-DD')
+      const responseSlots = await this.radiologyService.getModalitySlots(sectionId, reserveDate).toPromise()
+      return responseSlots.data || [];
+    }
+    return []
+  }
+
+  async fetchSlotData () {
+    const list: any = []
+    for (let item of this.days) {
+      const d = await this.getModalitySlots(item.date, this.sectionSelected.modality_hospital_id)
+      list.push(d)
+    }
+    this.slotData = list
+    // console.log('fetch list', list)
+    this.getSchedules()
+  }
+
+}
+
+class SlotWeeklyItem {
+  fromTime: any;
+  toTime: any;
+  date: Date;
+  patientName: any
+  patientDob: any
+  patientLocalMrNo: any
+  rowSpan: number = 0
+}
+class SlotWeeklyRow {
+  viewIndex: number;
+  fromTime: any;
+  toTime: any;
+  days: SlotWeeklyItem[];
 }
