b612dfaa3efca64642b955878f7e2f6f28e7488b
Ahmad Yasir <ahmad.yasir@siloamhospitals.com>
2022-07-28 10:52:20 +0700

    route to maintenance schedule

--- src/app/views/widgets/widget-modality-tab/widget-modality-tab.module.ts
+++ src/app/views/widgets/widget-modality-tab/widget-modality-tab.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {  RouterModule } from '@angular/router';
 
 import { ModalityTabComponent } from './widget-modality-tab.component';
 
@@ -11,6 +12,7 @@ import { ModalityTabComponent } from './widget-modality-tab.component';
   imports: [
     CommonModule,
     FormsModule,
+    RouterModule,
     ReactiveFormsModule,
   ],
   exports: [
