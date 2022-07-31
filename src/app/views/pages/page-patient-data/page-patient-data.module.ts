62408a8e51dc5c2adc48312c5c957287b1fa5c23
aisyahIlmy <aisyah.ilmy@bithealth.co.id>
2022-07-31 22:02:00 +0700

    basic layout for patient data

--- src/app/views/pages/page-patient-data/page-patient-data.module.ts
+++ src/app/views/pages/page-patient-data/page-patient-data.module.ts
@@ -2,11 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PagePatientDataComponent } from './page-patient-data.component';
 import { PagePatientDataRoutingModule } from './page-patient-data-routing.module';
+import { SectionHeaderModule } from '../../sections/section-header/section-header.module';
+import { SectionSidebarModule } from '../../sections/section-sidebar/section-sidebar.module';
+import { SectionFooterModule } from '../../sections/section-footer/section-footer.module';
 
 @NgModule({
   declarations: [PagePatientDataComponent],
   imports: [
     CommonModule,
+    SectionHeaderModule,
+    SectionSidebarModule,
+    SectionFooterModule,
     PagePatientDataRoutingModule
   ]
 })
