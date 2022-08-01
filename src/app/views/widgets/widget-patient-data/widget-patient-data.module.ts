37e34286a5d2e2bd2549a044c9b56161ee1192b2
dennisfilhan <filhandennis@gmail.com>
2022-08-01 12:01:26 +0700

    page-patient-data: update layout

--- src/app/views/widgets/widget-patient-data/widget-patient-data.module.ts
+++ src/app/views/widgets/widget-patient-data/widget-patient-data.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { WidgetPatientDataComponent } from './widget-patient-data.component';
 import { AutocompleteLibModule } from 'angular-ng-autocomplete';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [WidgetPatientDataComponent],
   imports: [
     CommonModule,
     NgSelectModule,
+    NgbAlertModule,
     AutocompleteLibModule
   ],
   exports: [
